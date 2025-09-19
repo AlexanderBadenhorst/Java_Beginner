4124ea2f6bbbcd01d533f2e1a7cee5ff11bf96c2
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2025-09-19 12:37:06 +0200

    chap10 additional comments

--- src/Chap10/Page303/WithStatic.java
+++ src/Chap10/Page303/WithStatic.java
@@ -1,13 +1,24 @@
 package Chap10.Page303;
 
-import static java.lang.Math.*;
+// -------------------------------
+// Example WITH static imports
+// -------------------------------
+
+// Import all static members of Math (sqrt, tan, etc.)
+// This lets us call sqrt() and tan() directly without "Math."
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.tan;
 import static java.lang.System.out;
 
 class WithStatic {
     public static void main(String[] args) {
+        // Print a header so we know which version is running
         out.println("With static import:");
+
+        // Thanks to static import, we can call methods directly:
+        // sqrt() instead of Math.sqrt(), tan() instead of Math.tan()
         out.println("sqrt(2.0) = " + sqrt(2.0));
         out.println("tan(60) = " + tan(60));
     }
 }
-
