862cc5ccc8e9a8448658048929459493f105767f
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2025-09-19 12:37:02 +0200

    chap10 additional comments

--- src/Chap10/Page303/StaticImportTest.java
+++ src/Chap10/Page303/StaticImportTest.java
@@ -1,9 +1,15 @@
 package Chap10.Page303;
 
-// Without static imports
+// -------------------------------
+// Example WITHOUT static imports
+// -------------------------------
 class NoStatic {
     public static void main(String[] args) {
+        // Print a header so we know which version is running
         System.out.println("Without static import:");
+
+        // Calling Math methods the usual way:
+        // - Must prefix with "Math." because sqrt() and tan() are static methods in java.lang.Math
         System.out.println("sqrt(2.0) = " + Math.sqrt(2.0));
         System.out.println("tan(60) = " + Math.tan(60));
     }
